195df209b9f10ef8633f22860cc9d99461c21763
MathisGuilhin <mathis.guilhin@insa-lyon.fr>
2019-08-21 14:50:43 +0200

    comments for install script

--- install.bat
+++ install.bat
@@ -1,27 +1,36 @@
+:: Install project dependencies
 yarn install
+:: Adding yalc
 yarn global add yalc
+:: Linking submodules
+:: 1/Cornerstone Tools
 cd cornerstoneTools
 yarn install
 yalc publish
 cd ..
 yarn install
 yalc add cornerstone-tools
+:: 2/Ohif-core
 cd ohif-core
 yarn install
 yalc publish
 cd ..
 yarn install
 yalc add ohif-core
+:: 3/React Viewerbase
 cd react-viewerbase
 yarn install
 yalc publish
 cd ..
 yarn install
 yalc add react-viewerbase
+:: 4/Ohif cornerstone extension
 cd extensions\ohif-cornerstone-extension
 yarn install
 yalc publish
 cd ..\..
 yarn install
 yalc add @ohif/extension-cornerstone
+:: run viewer
+yarn run dev
 
